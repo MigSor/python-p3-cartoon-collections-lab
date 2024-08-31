f25f022b23b920de3f179cc05d9259b77cad81d2
Miguel Soriano <miguel.sor.88@gmail.com>
2024-09-01 00:37:29 +1000

    completed lab using lists and list comprehensions

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,31 @@
-def roll_call_dwarves():
-    pass
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def roll_call_dwarves(dwarves_list):
+    x = 0
+    for dwarf in dwarves_list:
+        x += 1
+        print(f'{x}. {dwarf}')
 
-def find_the_cheese():
-    pass
+
+def summon_captain_planet(list):
+    capitalized = [element.capitalize() + "!" for element in list]
+    return capitalized
+
+
+def long_planeteer_calls(words):
+    for word in words:
+        if len(word) > 3:
+            return False
+        else:
+            return True
+
+
+def find_the_cheese(snacks_to_check):
+    types_of_chesse = ["gouda", "cheddar", "camembert"]
+    for cheese in types_of_chesse:
+        if cheese in snacks_to_check:
+
+            return cheese
+    else:
+
+        return None
